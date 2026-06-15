1feb84132c29103a504885b643762a3c829064ea
buddharajuvarmausa@gmail.com <buddharajuvarmausa@gmail.com>
2026-06-14 22:27:38 -0500

    Assignment

--- 11 Assignment1/assignment.js
+++ 11 Assignment1/assignment.js
@@ -59,3 +59,60 @@ const game = {
     team2: 6.5,
   },
 };
+
+//Solution
+
+//1. Create one player array for each team (variables 'players1' and 'players2')
+
+let [players1, players2] = game.players; //destructuring
+console.log("players1", players1);
+console.log("players2", players2);
+/*2. The first player in any player array is the wicketkeeper and the 
+others are field players. For India (team 1) create one variable ('Ind') 
+with the wicketkeeper name, and one array ('fieldPlayers') 
+with all the remaining 10 field players
+*/
+let [captain, ...fieldPlayers] = players1; //rest operator
+console.log("caption", captain);
+console.log("fieldPlayers", fieldPlayers);
+//3. Create an array 'allPlayers' containing all players of both teams (22 players)
+let allPlayers = [...players1, ...players2]; //spread operator
+console.log("allPlayers", allPlayers);
+
+/*4. During the game, India (team 1) used 3 substitute players. 
+So create a new array ('players1Final') containing all the original 
+team1 players plus 'Sandeep', 'John' and 'Sunil'
+*/
+let players1Final = [...players1, "Sandeep", "John", "Sunil"];
+console.log(players1Final);
+
+/*5. Based on the game.odds object, create one variable for each odd 
+(called 'team1', 'draw' and 'team2') */
+//let [team1, x, team2] = game.odds;
+//console.log(team1);
+let { team1, x: draw, team2 } = game.odds; //alias draw for x
+console.log(team1, draw, team2);
+
+/*6. The team with the lower odd is more likely to win. 
+Print to the console which team is more likely to win, 
+WITHOUT using an if/else statement or the ternary operator.
+*/
+team1 < team2 && console.log("Team 1 will win");
+team2 < team1 && console.log("Team 2 will win");
+/*7. Loop over the game.Century array and print each player name to the 
+console, along with the Century number (Example: "Century 1: Kohli")
+*/
+for (let [currItem, value] of game.Century.entries()) {
+  console.log(`Century ${currItem + 1}: ${value}`);
+}
+
+//8. Use a loop to calculate the average odd and log it to the console
+let sum = 0;
+let counter = 0;
+for (let currItem of Object.values(game.odds)) {
+  sum = sum + currItem;
+  counter++;
+}
+console.log(sum);
+console.log(counter);
+console.log("Average", Math.round(sum / counter));
